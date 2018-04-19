c6e71ee3e29ab142ed275938ced1af19d25d54a6
Felipe <fehmel@gmail.com>
2018-04-19 15:14:55 -0300

    Quase pronto

--- bin/8puzzlesolver.cpp
+++ bin/8puzzlesolver.cpp
@@ -25,6 +25,14 @@ Node* createNode(int *state, int depth, int cost, int action) {
 	return g;
 }
 
+void printState(int *state) {
+	for(int i = 0; i < PUZZLESIZE; i++) {
+		cout << state[i] << " ";
+	}
+
+	cout << endl;
+}
+
 /*
 1: cima
 2: baixo
@@ -35,7 +43,7 @@ Node* createNode(int *state, int depth, int cost, int action) {
 int* createState(int* currentState, int blank, int action) {
 	int aux;
 	int* newState = (int*) malloc(sizeof(int)*(PUZZLESIZE));
-	
+
 	//duplicar estado anterior
 	for(int i = 0; i < PUZZLESIZE; i++) {
 			newState[i] = currentState[i];
@@ -47,6 +55,7 @@ int* createState(int* currentState, int blank, int action) {
 			aux = newState[blank-3];
 			newState[blank-3] = 0;
 			newState[blank] = aux;
+			//printState(newState);
 			return newState;
 		}
 	}
@@ -56,6 +65,7 @@ int* createState(int* currentState, int blank, int action) {
 			aux = newState[blank+3];
 			newState[blank+3] = 0;
 			newState[blank] = aux;
+			//printState(newState);
 			return newState;
 		}
 	}
@@ -65,6 +75,7 @@ int* createState(int* currentState, int blank, int action) {
 			aux = newState[blank - 1];
 			newState[blank-1] = 0;
 			newState[blank] = aux;
+			//printState(newState);
 			return newState;
 		}
 	}
@@ -74,6 +85,7 @@ int* createState(int* currentState, int blank, int action) {
 			aux = newState[blank + 1];
 			newState[blank+1] = 0;
 			newState[blank] = aux;
+			//printState(newState);
 			return newState;
 		}
 	}
@@ -81,34 +93,68 @@ int* createState(int* currentState, int blank, int action) {
 	return newState;
 }
 
-void printState(int *state) {
-	for(int i = 0; i < PUZZLESIZE; i++) {
-		cout << state[i] << " ";
+int compareStates(int* newState, int* closeStates) {
+	int count = 0;
+
+	for(int i = 0; i < PUZZLESIZE-1; i++) {
+		if(newState[i] == closeStates[i]) count++;
 	}
 
-	cout << endl;
-}
-
-int isFinalState(Node* initialNode, int* finalState) {
-	int count=0;
-	for(int i = 0; i < PUZZLESIZE; i++) {
-		if(initialNode->state[i] == finalState[i]) count++;
-	}
-
-	if(count == PUZZLESIZE) return 1;
+	if(count == PUZZLESIZE-1) return 1;	
 	else return 0;
+
 }
-/*
+
+vector<Node*> *geraFilhos(Node* currentNode, vector<Node*>*closeStates) {
+	int blank;
+	int* newState = (int*) malloc(sizeof(int)*(PUZZLESIZE));
+	int action[4] = { 0, 0, 0, 0 };
+	int size = closeStates->size();
+	Node* nodo_filho = new Node;
+	vector<Node*> *filhos = new vector<Node*>;
+
+	for(int i = 0; i < PUZZLESIZE; i++) {
+		if(currentNode->state[i] == 0) blank = i;
+	}
+
+	//cout << closeStates->size() << endl;
+
+	if(blank - 3 >= 0) action[0] = 1;
+	if(blank + 3 < PUZZLESIZE) action[1] = 2;
+	if(blank % 3 > 0) action[2] = 3;
+	if(blank < PUZZLESIZE-1 && (blank % 3) < 2) action[3] = 4;
+
+	for(int i = 0; i < 4; i++) {
+
+		if(action[i] != 0) {
+			newState = createState(currentNode->state, blank, action[i]);
+			
+			for(int j = 0; j < size; j++) {
+				if(!compareStates(newState, closeStates->at(j)->state)) {
+					nodo_filho = createNode(newState, 0, currentNode->cost+1, action[i]);
+
+					filhos->push_back(nodo_filho);
+					closeStates->push_back(nodo_filho);
+				}
+			}
+		}
+	}
+
+	return filhos;
+
+}
+
 int bfs(Node *initialNode, int* finalState) {
-	Node* currentState;
+	Node* currentNode;
 	deque<Node*> fila;
 
-	vector<Node*> *closeStates;
-	vector<Node*> *filhos;
+	vector<Node*> *closeStates = new vector<Node*>;
+	vector<Node*> *filhos = new vector<Node*>;
 
 	fila.push_back(initialNode);
+	closeStates->push_back(initialNode);
 
-	if(isFinalState(initialNode, finalState)) {
+	if(compareStates(initialNode->state, finalState)) {
 		return 1;
 	}
 
@@ -116,10 +162,13 @@ int bfs(Node *initialNode, int* finalState) {
 		currentNode = fila.front();
 		fila.pop_front();
 
-		filhos = geraFilhos(currentNode, closeStates);
+		if(!closeStates->empty()) {		
+			filhos = geraFilhos(currentNode, closeStates);
+		}
 
 		for(int i = 0; i < filhos->size(); i++) {
-			if(isFinalState(filhos->at(i)->state, finalState)) {
+			if(compareStates(filhos->at(i)->state, finalState)) {
+				cout << filhos->at(i)->cost << endl;
 				return 1;
 			}else{
 				fila.push_back(filhos->at(i));
@@ -127,15 +176,17 @@ int bfs(Node *initialNode, int* finalState) {
 		}
 	}
 	return 0;
-}*/
+}
 
 int main() {
-	int initialState[PUZZLESIZE] = {1, 6, 7, 2, 5, 3, 4, 0, 8};
+	//int initialState[PUZZLESIZE] = {1, 6, 7, 2, 5, 3, 4, 0, 8};
+	int initialState[PUZZLESIZE] = {3, 1, 2,
+									6, 4, 5,
+									7, 0, 8};
 	int finalState[PUZZLESIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-	printState(initialState);
-
 	Node *initialNode = createNode(initialState, 0, 0, -1);
 
-	int *newState = createState(initialState, 7, 1);
-	printState(newState);
+	if(bfs(initialNode, finalState)) cout << "Encontrei o finalState" << endl;
+	else cout << "Não encontrei o finalState" << endl;
+
 }
